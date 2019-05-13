1f0e8d89ab772d1927dcf20b86f047dc27ae9524
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2019-05-13 10:49:00 -0700

    Made changed to kitsilano scene. Hovering over a selector will now change its colour.

--- Assets/OFC Stuff/Scripts/kits_scene/selector.cs
+++ Assets/OFC Stuff/Scripts/kits_scene/selector.cs
@@ -5,10 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class selector : MonoBehaviour
 {
+    private Renderer renderer;
+
+    // Default colour of selector
+    Color defaultColor = new Color32(255, 0, 0, 60);
+    // Colour that is displayed when selector is hovered over 
+    Color hoverColor = new Color32(69, 255, 0, 60);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -21,4 +28,15 @@ public class selector : MonoBehaviour
     void OnMouseDown() {
     	SceneManager.LoadScene("block_scene");
     }
+
+    void OnMouseEnter()
+    { 
+        renderer.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        renderer.material.color = defaultColor;
+    }
+
 }
