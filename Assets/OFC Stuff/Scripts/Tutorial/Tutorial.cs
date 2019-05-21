02fd024b2883f61b947cc249eb5b90ff025497b9
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2019-05-21 11:40:06 -0700

    Added a tutorial demo

---
+++ Assets/OFC Stuff/Scripts/Tutorial/Tutorial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+
+    public int Order;
+
+    public string Explanation;
+
+    void Awake()
+    {
+        // Add all the tutorials within the scene to the tutorial manager
+        TutorialManager.Instance.Tutorials.Add(this);
+    }
+
+    public virtual void CheckIfHappening() { }
+}
