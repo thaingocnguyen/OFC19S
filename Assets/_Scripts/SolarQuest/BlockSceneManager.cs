26afc31c18143df9d1b5709e7feefb5bcf66d290
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2019-06-06 13:50:02 -0700

    Camera switch when house selected

--- Assets/_Scripts/SolarQuest/BlockSceneManager.cs
+++ Assets/_Scripts/SolarQuest/BlockSceneManager.cs
@@ -5,15 +5,62 @@ using UnityEngine;
 public class BlockSceneManager : MonoBehaviour
 {
     [SerializeField] InfoPanel infoPanel;
+    [SerializeField] Camera mainCamera;
+    [SerializeField] Camera[] houseCameras;
+
+    // UI ELEMENTS
+    [SerializeField] GameObject backButton;
+
     // Start is called before the first frame update
     void Start()
     {
+        // SET UP OF CAMERAS
+        foreach (Camera c in houseCameras) { c.gameObject.SetActive(false); }
+        mainCamera.enabled = true;
+
+        // SET UP OF SCENE 
+        backButton.SetActive(false);
+
         infoPanel.ShowInfoPanel();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void UseCamera(int cameraIndex)
+    {
+        if (cameraIndex >= houseCameras.Length)
+        {
+            Debug.Log("No such camera");
+            return;
+        }
+
+        GetComponent<HouseSelector>().MapView = false;
+        for (int i = 0; i < houseCameras.Length; i++)
+        {
+            if (i == cameraIndex)
+            {
+                houseCameras[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                houseCameras[i].gameObject.SetActive(false);
+            }
+        }
+
+        mainCamera.gameObject.SetActive(false);
+        backButton.SetActive(true);
+    }
+
+    public void ShowMap()
+    {
+        GetComponent<HouseSelector>().MapView = true;
+        mainCamera.gameObject.SetActive(true);
+        backButton.SetActive(false);
+        foreach (Camera c in houseCameras) { c.gameObject.SetActive(false); }
+    }
+
 }
--- Assets/_Scripts/SolarQuest/HouseSelector.cs
+++ Assets/_Scripts/SolarQuest/HouseSelector.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class HouseSelector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    private int selectedHouse;
+    private bool mapView = true;
+
+    [SerializeField] Animator selectBoxAnimator;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && mapView)
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -24,37 +24,55 @@ public class HouseSelector : MonoBehaviour
                 {
                     // Center camera on house and display popup house info and select house button
                     CameraCenter(hit.transform.gameObject);
+                    selectBoxAnimator.SetBool("IsOnScreen", true);
                 }
             }
         }
     }
 
+    public bool MapView
+    {
+        get { return mapView; }
+        set { mapView = value; }
+    }
+
     void CameraCenter(GameObject house)
     {
-
         switch (house.name)
         {
             case "House1":
                 Camera.main.transform.position = new Vector3(-34f, Camera.main.transform.position.y, Camera.main.transform.position.z);
+                selectedHouse = 0;
                 break;
             case "House2":
                 Camera.main.transform.position = new Vector3(-30f, Camera.main.transform.position.y, Camera.main.transform.position.z);
+                selectedHouse = 1;
                 break;
             case "House3":
                 Camera.main.transform.position = new Vector3(1f, Camera.main.transform.position.y, Camera.main.transform.position.z);
+                selectedHouse = 2;
                 break;
             case "House4":
                 Camera.main.transform.position = new Vector3(27f, Camera.main.transform.position.y, Camera.main.transform.position.z);
+                selectedHouse = 3;
                 break;
             case "House5":
                 Camera.main.transform.position = new Vector3(58f, Camera.main.transform.position.y, Camera.main.transform.position.z);
+                selectedHouse = 4;
                 break;
             case "House6":
                 Camera.main.transform.position = new Vector3(62f, Camera.main.transform.position.y, Camera.main.transform.position.z);
+                selectedHouse = 5;
                 break;
             default:
                 break;
         }
+    }
+
+    public void SelectHouse()
+    {
+        selectBoxAnimator.SetBool("IsOnScreen", false);
+        GetComponent<BlockSceneManager>().UseCamera(selectedHouse);
     }
 
 }
