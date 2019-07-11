c7f4c416f8099ce28fd98d4aa627f4e2002026c8
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2019-07-11 10:20:45 -0700

    UF Quest demo ver 2

---
+++ Assets/_Scripts/UrbanForestryQuest/DraggableObject.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelEditor
+{
+    public class DraggableObject : MonoBehaviour
+    {
+        Vector3 initialPosition;
+        Quaternion initialRotation;
+        Vector3 mousePosition;
+        Vector3 worldPosition;
+
+        Level_Object objProperties;
+        GameObject cloneObj;
+        Node curNode;
+
+        GridBase gridBase;
+        LevelManager manager;
+
+        bool placed;
+        [SerializeField] GameObject objectToInstantiate;
+
+        private void Start()
+        {
+            manager = LevelManager.GetInstance();
+            placed = false;
+            initialPosition = gameObject.transform.position;
+            initialRotation = gameObject.transform.rotation;
+        }
+
+        void OnMouseDown()
+        {
+            if (!placed)
+            {
+                cloneObj = Instantiate(objectToInstantiate, initialPosition, initialRotation);
+            }
+            else
+            {
+                cloneObj = gameObject;
+            }
+            
+        }
+
+        void UpdateMousePosition()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                mousePosition = hit.point;
+            }
+        }
+
+        void OnMouseDrag()
+        {
+            UpdateMousePosition();
+            curNode = GridBase.GetInstance().NodeFromWorldPosition(mousePosition);
+            worldPosition = curNode.vis.transform.position;
+            cloneObj.transform.position = worldPosition;
+        }
+
+        void OnMouseUp()
+        {
+            if (curNode.placedObj != null)
+            {
+                manager.inSceneGameObjects.Remove(curNode.placedObj.gameObject);
+                Destroy(curNode.placedObj.gameObject);
+                curNode.placedObj = null;
+            }
+
+            curNode.placedObj = cloneObj.GetComponent<Level_Object>();
+            manager.inSceneGameObjects.Add(cloneObj);
+            placed = true;
+        }
+
+    }
+}
+
--- Assets/_Scripts/UrbanForestryQuest/LevelCreator.cs
+++ Assets/_Scripts/UrbanForestryQuest/LevelCreator.cs
@@ -10,6 +10,11 @@ namespace LevelEditor
         GridBase gridBase;
         InterfaceManager ui;
 
+        bool placeModeOn;
+        bool objMoving;
+        GameObject currentObject;
+        Node curNode;
+
         // Place obj variables
         bool hasObj;
         GameObject objToPlace;
@@ -55,8 +60,8 @@ namespace LevelEditor
         private void Update()
         {
             PlaceObject();
-            //PaintTile();
             DeleteObjs();
+            //PaintTile();
             //PlaceStackedObj();
             //CreateWall();
             //DeleteStackedObjs();
@@ -74,53 +79,120 @@ namespace LevelEditor
             }
         }
 
-        #region Place Objects
-        void PlaceObject()
+        public void PlaceModeToggle()
         {
-            if (hasObj)
+            if (!placeModeOn)
             {
-                UpdateMousePosition();
-
-                Node curNode = gridBase.NodeFromWorldPosition(mousePosition);
-
-                worldPosition = curNode.vis.transform.position;
-
-                if (cloneObj == null)
+                placeModeOn = true;
+                PassGameObjectToPlace("tree_small");
+            }
+            else
+            {
+                placeModeOn = false;
+                if (cloneObj != null)
                 {
-                    cloneObj = Instantiate(objToPlace, worldPosition, Quaternion.identity) as GameObject;
-                    objProperties = cloneObj.GetComponent<Level_Object>();
-                }
-                else
-                {
-                    cloneObj.transform.position = worldPosition;
-
-                    // Placing object
-                    if (Input.GetMouseButton(0) && !ui.mouseOverUIElement)
-                    {
-                        // If current node have a placed object
-                        if (curNode.placedObj != null)
-                        {
-                            manager.inSceneGameObjects.Remove(curNode.placedObj.gameObject);
-                            Destroy(curNode.placedObj.gameObject);
-                            curNode.placedObj = null;
-                        }
-
-                        GameObject actualObjPlaced = Instantiate(objToPlace, worldPosition, cloneObj.transform.rotation) as GameObject;
-                        Level_Object placedObjProperties = actualObjPlaced.GetComponent<Level_Object>();
-
-                        placedObjProperties.gridPosX = curNode.nodePosX;
-                        placedObjProperties.gridPosZ = curNode.nodePosZ;
-                        curNode.placedObj = placedObjProperties;
-                        manager.inSceneGameObjects.Add(actualObjPlaced);
-                    }
-
-                    if (Input.GetMouseButtonUp(1))
-                    {
-                        objProperties.ChangeRotation();
-                    }
+                    Debug.Log("Saved tree");
+                    manager.inSceneGameObjects.Add(cloneObj);
+                    cloneObj = null;
                 }
             }
         }
+
+        #region Place Objects
+        //void PlaceObject()
+        //{
+        //    if (hasObj)
+        //    {
+        //        UpdateMousePosition();
+
+        //        Node curNode = gridBase.NodeFromWorldPosition(mousePosition);
+
+        //        worldPosition = curNode.vis.transform.position;
+
+        //        if (cloneObj == null)
+        //        {
+        //            cloneObj = Instantiate(objToPlace, worldPosition, Quaternion.identity) as GameObject;
+        //            objProperties = cloneObj.GetComponent<Level_Object>();
+        //        }
+        //        else
+        //        {
+        //            cloneObj.transform.position = worldPosition;
+
+        //            // Placing object
+        //            if (Input.GetMouseButton(0) && !ui.mouseOverUIElement)
+        //            {
+        //                // If current node have a placed object
+        //                if (curNode.placedObj != null)
+        //                {
+        //                    manager.inSceneGameObjects.Remove(curNode.placedObj.gameObject);
+        //                    Destroy(curNode.placedObj.gameObject);
+        //                    curNode.placedObj = null;
+        //                }
+
+        //                GameObject actualObjPlaced = Instantiate(objToPlace, worldPosition, cloneObj.transform.rotation) as GameObject;
+        //                Level_Object placedObjProperties = actualObjPlaced.GetComponent<Level_Object>();
+
+        //                placedObjProperties.gridPosX = curNode.nodePosX;
+        //                placedObjProperties.gridPosZ = curNode.nodePosZ;
+        //                curNode.placedObj = placedObjProperties;
+        //                manager.inSceneGameObjects.Add(actualObjPlaced);
+        //            }
+
+        //            if (Input.GetMouseButtonUp(1))
+        //            {
+        //                objProperties.ChangeRotation();
+        //            }
+        //        }
+        //    }
+        //}
+
+        void PlaceObject()
+        {
+            if (placeModeOn)
+            {
+                // When mouse is held down
+                if (Input.GetMouseButton(0) && !ui.mouseOverUIElement)
+                {
+                    // Get node on grid from mouse position
+                    UpdateMousePosition();
+                    curNode = gridBase.NodeFromWorldPosition(mousePosition);
+                    worldPosition = curNode.vis.transform.position;
+
+                    // If object hasn't been instantiated then instantiate it
+                    if (cloneObj == null)
+                    {
+                        cloneObj = Instantiate(objToPlace, worldPosition, Quaternion.identity) as GameObject;
+                        objProperties = cloneObj.GetComponent<Level_Object>();
+                    }
+                    // Else change position of object to mouse position
+                    else
+                    {
+                        cloneObj.transform.position = worldPosition;
+                    }
+                }
+                // When mouse is released and there is a clone object 
+                if (Input.GetMouseButtonUp(0) && cloneObj != null && !ui.mouseOverUIElement)
+                {
+                    // If current node have a placed object remove clone object, do not allow object to be placed
+                    if (curNode.placedObj != null)
+                    {
+                        // Remove clone object
+                        Destroy(cloneObj);
+                        cloneObj = null;
+                        objProperties = null;
+                    }
+                    else
+                    {
+                        
+                        objProperties.gridPosX = curNode.nodePosX;
+                        objProperties.gridPosZ = curNode.nodePosZ;
+                        
+                    }
+                }
+
+            }
+        }
+
 
         public void PassGameObjectToPlace(string objId)
         {
@@ -134,6 +206,8 @@ namespace LevelEditor
             cloneObj = null;
             objToPlace = ResourceManager.GetInstance().GetObjBase(objId).objPrefab;
         }
+
+
 
         void DeleteObjs()
         {
