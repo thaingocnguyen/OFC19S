128ec7ee4eb13c9d2fc9d63b317fbb95178f87b2
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2019-07-25 15:53:15 -0700

    Finished revamping solar quest tutorial

--- Assets/_Scripts/UrbanForestryQuest/InfoBox.cs
+++ Assets/_Scripts/UrbanForestryQuest/InfoBox.cs
@@ -4,67 +4,64 @@ using UnityEngine;
 using TMPro;
 
 
-namespace UrbanForestryQuest
+
+public abstract class InfoBox : MonoBehaviour
 {
-    public abstract class InfoBox : MonoBehaviour
+
+    [SerializeField] protected TextMeshProUGUI displayedText;
+    protected Queue<string> sentences;
+
+    [SerializeField] float typeSpeed = 0.05f;
+
+    bool displayingSentence;
+    [SerializeField] bool debugMode;
+
+
+    private void Awake()
     {
-
-        [SerializeField] protected TextMeshProUGUI displayedText;
-        protected Queue<string> sentences;
-
-        [SerializeField] float typeSpeed;
-
-        bool displayingSentence;
-        [SerializeField] bool debugMode;
-
-
-        private void Awake()
-        {
-            sentences = new Queue<string>();
-        }
-
-        protected void LoadText()
-        {
-            sentences.Clear();
-
-            foreach (string sentence in sentences)
-            {
-                sentences.Enqueue(sentence);
-            }
-
-            DisplayNextSentence();
-        }
-
-        public void DisplayNextSentence()
-        {
-            if (!displayingSentence || debugMode)
-            {
-                if (sentences.Count == 0)
-                {
-                    HandleNoSentencesLeft();
-                    return;
-                }
-
-                string sentence = sentences.Dequeue();
-                StopAllCoroutines();
-                StartCoroutine(TypeSentence(sentence));
-                //displayedText.text = sentences.Dequeue();
-            }
-        }
-
-        IEnumerator TypeSentence(string sentence)
-        {
-            displayingSentence = true;
-            displayedText.text = "";
-            foreach (char letter in sentence)
-            {
-                displayedText.text += letter;
-                yield return new WaitForSeconds(typeSpeed);
-            }
-            displayingSentence = false;
-        }
-
-        public abstract void HandleNoSentencesLeft();
+        sentences = new Queue<string>();
     }
 
+    protected void LoadText()
+    {
+        sentences.Clear();
+
+        foreach (string sentence in sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (!displayingSentence || debugMode)
+        {
+            if (sentences.Count == 0)
+            {
+                HandleNoSentencesLeft();
+                return;
+            }
+
+            string sentence = sentences.Dequeue();
+            StopAllCoroutines();
+            StartCoroutine(TypeSentence(sentence));
+        }
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        displayingSentence = true;
+        displayedText.text = "";
+        foreach (char letter in sentence)
+        {
+            displayedText.text += letter;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+        displayingSentence = false;
+    }
+
+    public abstract void HandleNoSentencesLeft();
 }
+
